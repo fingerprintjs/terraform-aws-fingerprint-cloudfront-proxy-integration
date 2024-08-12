1be4645f482fc6dd22e27f35a05a4cfc7a3a5b9c
Necip Allef <necipallef@gmail.com>
2024-08-12 17:59:58 +0300

    chore: add fpjs_proxy_lambda_role_permissions_boundary_arn to examples

--- examples/existing-ditribution/fingerprint.tf
+++ examples/existing-ditribution/fingerprint.tf
@@ -4,4 +4,5 @@ module "fingerprint_cloudfront_integration" {
   fpjs_agent_download_path = var.fpjs_agent_download_path
   fpjs_get_result_path     = var.fpjs_get_result_path
   fpjs_shared_secret       = var.fpjs_shared_secret
+  // fpjs_proxy_lambda_role_permissions_boundary_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
 }
