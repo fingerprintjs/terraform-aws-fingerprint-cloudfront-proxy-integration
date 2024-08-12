74506f27064749cf3cdefbca38105c6232f61898
Juraj Uhlar <juro.uhlar@gmail.com>
2024-08-12 17:13:11 +0200

    chore: add comments about permissions boundary

--- examples/existing-ditribution/fingerprint.tf
+++ examples/existing-ditribution/fingerprint.tf
@@ -4,5 +4,7 @@ module "fingerprint_cloudfront_integration" {
   fpjs_agent_download_path = var.fpjs_agent_download_path
   fpjs_get_result_path     = var.fpjs_get_result_path
   fpjs_shared_secret       = var.fpjs_shared_secret
+
+  // You can define the proxy function's [permission boundary](https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_boundaries.html)
   // fpjs_proxy_lambda_role_permissions_boundary_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
 }
