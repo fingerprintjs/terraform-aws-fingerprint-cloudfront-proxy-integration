82c09ab57c2240dee6f460c7ec8befe6272912ee
Juraj Uhlar <juro.uhlar@gmail.com>
2024-08-13 16:38:29 +0200

    chore: better describe permission boundary input

--- examples/standalone-distribution/fingerprint.tf
+++ examples/standalone-distribution/fingerprint.tf
@@ -6,5 +6,6 @@ module "fingerprint_cloudfront_integration" {
   fpjs_shared_secret       = var.fpjs_shared_secret
 
   // You can define the proxy function's [permission boundary](https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_boundaries.html)
-  // fpjs_proxy_lambda_role_permissions_boundary_arn = "arn:aws:iam::aws:policy/AWSLambda_FullAccess"
+  // See https://dev.fingerprint.com/docs/aws-cloudfront-integration-via-terraform#defining-a-permission-boundary-for-the-proxy-function
+  // fpjs_proxy_lambda_role_permissions_boundary_arn = "arn:aws:iam::<YOUR_ACCOUNT_ID>:policy/YOUR_POLICY_NAME"
 }
